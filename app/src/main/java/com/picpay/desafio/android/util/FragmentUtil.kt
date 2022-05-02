78832810fd616e60124413d8f0ad13d788310e35
Freitas <rodrigo.b.freitas@capgemini.com>
2022-05-02 18:33:06 -0300

    Criando as classes FragmentUtil e NetworkUtil

---
+++ app/src/main/java/com/picpay/desafio/android/util/FragmentUtil.kt
@@ -0,0 +1,31 @@
+package com.picpay.desafio.android.util
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+class FragmentUtil {
+    companion object {
+        fun changeFragment(
+            id: Int,
+            fragment: Fragment,
+            fragmentManager: FragmentManager,
+            backstack: Boolean,
+            bundle: Bundle?
+        ) {
+            val transaction = fragmentManager
+                .beginTransaction()
+
+            transaction.replace(id, fragment)
+
+            if (backstack) {
+                transaction.addToBackStack(null)
+            }
+            if (bundle != null) {
+                fragment.setArguments(bundle)
+            }
+
+            transaction.commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/picpay/desafio/android/util/NetworkUtil.kt
@@ -0,0 +1,17 @@
+package com.picpay.desafio.android.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+class NetworkUtil {
+    companion object {
+        fun checkConnection(context: Context): Boolean {
+            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+            val mobileIsConnected = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)!!.isConnected
+            val wifiIsConnected = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI)!!.isConnected
+
+            return !(!mobileIsConnected && !wifiIsConnected)
+        }
+    }
+}
