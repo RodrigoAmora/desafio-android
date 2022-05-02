a819eb95863c6954f1f90e9db8451e83839746d0
Freitas <rodrigo.b.freitas@capgemini.com>
2022-05-02 18:34:25 -0300

    Método inject(userDataSource: UserDataSource)

--- app/src/main/java/com/picpay/desafio/android/component/PicPayComponent.kt
+++ app/src/main/java/com/picpay/desafio/android/component/PicPayComponent.kt
@@ -1,10 +1,14 @@
 package com.picpay.desafio.android.component
 
-import com.picpay.desafio.android.MainActivity
+import com.picpay.desafio.android.datasource.UserDataSource
+import com.picpay.desafio.android.ui.MainActivity
 import com.picpay.desafio.android.module.PicPayModule
+import com.picpay.desafio.android.ui.user.UserViewModel
 import dagger.Component
 
 @Component(modules = [PicPayModule::class])
 interface PicPayComponent {
     fun inject(mainActivity: MainActivity)
+    fun inject(userVIewModel: UserViewModel)
+    fun inject(userDataSource: UserDataSource)
 }
